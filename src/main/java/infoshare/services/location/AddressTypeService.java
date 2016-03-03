0f39802c98e00a76af4834696ae50df05d2cd7cc
Thulebona <thulehadebe@outlook.com>
2016-03-03 16:42:37 +0200

    restAPI and services 90% done

--- src/main/java/infoshare/services/location/AddressTypeService.java
+++ src/main/java/infoshare/services/location/AddressTypeService.java
@@ -1,12 +1,8 @@
 package infoshare.services.location;
 
-import infoshare.app.conf.RestUtil;
 import infoshare.domain.location.AddressType;
-import infoshare.restapi.location.LocationBaseUrl;
 import infoshare.services.Services;
 import org.springframework.stereotype.Component;
-
-import java.util.Set;
 
 /**
  * Created by user9 on 2016/03/01.
